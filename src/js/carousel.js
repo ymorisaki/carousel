95d5cf770550c80e58d7ff0e746ccfedeb412c4f
yuji-m <yuji-m@mitsue.co.jp>
2019-04-04 18:32:32 +0900

    途中経過

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -6,6 +6,7 @@ export function setCarousel() {
 
   initCarousel('.js-carousel', {
     autoPlay: true,
+    column: 3,
     // animationType: 'fade',
   });
 
@@ -60,7 +61,7 @@ export function setCarousel() {
         this.wrap = root.querySelector('.' + o.wrap);
         this.item = root.querySelectorAll('.' + o.item);
         this.itemLength = this.item.length;
-        this.itemFocus = this.item.forEach(function (el) {
+        this.itemFocus += this.item.forEach(function (el) {
           el.querySelectorAll(FOCUSABLE);
         });
 
@@ -129,6 +130,7 @@ export function setCarousel() {
           this.setInitItems();
           this.cloneSlider();
           this.setController();
+          this.setColItems();
         },
 
         /**
@@ -143,6 +145,7 @@ export function setCarousel() {
             for (; i < cl.length; i++) {
               el.classList.add(cl[i]);
             }
+
             el.setAttribute('type', 'button');
             el.appendChild(span);
             span.textContent = txt;
@@ -294,20 +297,274 @@ export function setCarousel() {
         setColItems: function () {
           const self = this;
           let i = 0;
-          let colMargin;
+          let maxLength = this.itemLength;
+          let styles = null;
+          let elWidth = null;
 
           if (this.isSliding) {
             return;
           }
 
           if (this.animationType === 'slide') {
+            // カラムオプション変更とリサイズによるアイテム幅の計算
             if (this.column === 1) {
               this.item.forEach(function (el) {
                 el.style.width = '100%';
               });
+
+              this.cloneBeforeItem.forEach(function (el) {
+                el.style.width = '100%';
+              });
+
+              this.cloneAfterItem.forEach(function (el) {
+                el.style.width = '100%';
+              });
+
+              styles = window.getComputedStyle(this.item[0]);
+              elWidth = this.item[0].getBoundingClientRect().width;
+              this.colMargin = parseInt(styles.marginRight.replace(/px/, ''));
+            } else {
+              this.item.forEach(function (el) {
+                el.style.width = 'calc(' + (100 / self.column) + '% - ' + (self.colMargin / self.column * (self.column - 1)) + 'px)';
+              });
+
+              this.cloneBeforeItem.forEach(function (el) {
+                el.style.width = 'calc(' + (100 / self.column) + '% - ' + (self.colMargin / self.column * (self.column - 1)) + 'px)';
+              });
+
+              this.cloneAfterItem.forEach(function (el) {
+                el.style.width = 'calc(' + (100 / self.column) + '% - ' + (self.colMargin / self.column * (self.column - 1)) + 'px)';
+              });
+
+              styles = window.getComputedStyle(this.item[0]);
+              elWidth = this.item[0].getBoundingClientRect().width;
+              this.colMargin = parseInt(styles.marginRight.replace(/px/, ''));
+              this.itemWidth = elWidth + this.colMargin;
             }
           }
-        }
+
+          // クローンしたパネルの配置
+          for (; i < maxLength; i++) {
+            this.item[i].style.left = (this.itemWidth * i) + 'px';
+            this.cloneBeforeItem[i].style.left = (this.itemWidth * i) + 'px';
+            this.cloneAfterItem[i].style.left = (this.itemWidth * i) + 'px';
+          }
+
+          // クローンしたパネルのラッパーを左右に配置
+          this.cloneBeforeWrap.style.left = '-' + (this.itemWidth * this.itemLength) + 'px';
+          this.cloneAfterWrap.style.left = (this.itemWidth * this.itemLength) + 'px';
+
+          // スライド全体の再配置（動作未確認注意）
+          if (this.isCurrentNum !== 1) {
+            const promiseFunc = function () {
+              return new Promise(function (resolve) {
+                this.slideInner.style.left = '-' + (this.itemWidth * (this.isCurrentNum - 1)) + 'px';
+                this.slideInner.style.transitionDuration = '0s';
+                styles = window.getComputedStyle(this.slideInner);
+                resolve();
+              });
+            };
+            promiseFunc().then(function () {
+              self.nowPosition = parseInt(styles.left.match(/(\d+)/)[0], 10);
+            });
+          }
+        },
+
+        /**
+         * 一つ先にスライドする処理
+         * @return {void}
+         */
+        nextSlide: function () {
+          let styles = null;
+          let elWidth = null;
+
+          if (this.isSliding) {
+            return;
+          }
+
+          this.isSliding = true;
+          this.isCurrentNum++;
+          this.resizeBeforeWidth = window.innerWidth;
+
+          if (this.animationType === 'slide') {
+            styles = window.getComputedStyle(this.item[0]);
+            elWidth = el.getBoundingClientRect().width;
+            this.colMargin = parseInt(styles.marginRight.replace(/px/, ''));
+            this.itemWidth = elWidth + this.colMargin;
+
+            this.slideInner.style.left = '-' + (this.itemWidth + this.nowPosition) + 'px';
+
+            // 無限ループ時にインジケーターを最初に戻す
+            if (this.isCurrentNum === this.itemLength + 1) {
+              this.indicatorUpdate(0);
+
+              return;
+            }
+          }
+
+          if (this.animationType === 'fade') {
+            this.item[this.isCurrentNum - 2].style.opacity = 0;
+            this.item[this.isCurrentNum - 1].style.opacity = 1;
+
+            // 無限ループ時
+            if (this.isCurrentNum === this.itemLength + 1) {
+              this.item[this.isCurrentNum - 1].style.opacity = 0;
+              this.item[0].style.opacity = 1;
+              this.indicatorUpdate(0);
+              this.isCurrentNum = 1;
+
+              return;
+            }
+          }
+
+          // インジケーター同期
+          this.indicatorUpdate(this.isCurrentNum - 1);
+        },
+
+        /**
+         * 一つ先にスライドする処理（無限ループ切り替え時）
+         * @return {void}
+         */
+        nextInfiniteLoop: function () {
+          const self = this;
+
+          if (this.animationType === 'slide') {
+            this.slideInner.style.transitionDuration = '0s';
+
+            setTimeout(function () {
+              self.slideInner.style.left = 0;
+            }, 20);
+
+            // 現在のカレントとleft位置を初期化
+            this.isCurrentNum = 1;
+            this.nowPosition = 0;
+
+            setTimeout(function () {
+              self.slideInner.style.transitionDuration = (self.duration / 1000) + 's';
+              self.isSliding = false;
+            }, 40);
+          }
+        },
+
+        /**
+         * 一つ前にスライドする処理
+         * @return {void}
+         */
+        prevSlide: function () {
+          let styles = null;
+          let elWidth = null;
+
+          if (this.isSliding) {
+            return;
+          }
+
+          this.isSliding = true;
+          this.isCurrentNum--;
+          this.resizeBeforeWidth = window.innerWidth;
+
+          if (this.animationType === 'slide') {
+            styles = window.getComputedStyle(this.item[0]);
+            elWidth = this.item[0].getBoundingClientRect().width;
+            this.colMargin = parseInt(styles.marginRight.replace(/px/, ''));
+            this.itemWidth = elWidth + this.colMargin;
+
+            if (this.isCurrentNum === 0) {
+              this.slideInner.style.left = (this.nowPosition + this.itemWidth) + 'px';
+            } else if (this.isCurrentNum === 1) {
+              this.slideInner.style.left = 0;
+            } else {
+              this.slideInner.style.left = '-' + (this.nowPosition - this.itemWidth) + 'px';
+            }
+          }
+
+          if (this.animationType === 'fade') {
+            if (this.isCurrentNum === 0) {
+              this.item[0].style.opacity = 0;
+              this.item[this.itemLength - 1].style.opacity = 1;
+              this.isCurrentNum = this.itemLength;
+            } else {
+              this.item[this.isCurrentNum].style.opacity = 0;
+              this.item[this.isCurrentNum - 1].style.opacity = 1;
+            }
+          }
+
+          // インジケーター同期
+          if (this.isCurrentNum === 0) {
+            this.indicatorUpdate(this.itemLength - 1);
+          } else {
+            this.indicatorUpdate(this.isCurrentNum - 1);
+          }
+        },
+
+        /**
+         * 一つ前にスライドする処理（無限ループ切り替え時）
+         * @return {void}
+         */
+        prevInfiniteLoop: function () {
+          const self = this;
+          let targetPosition = null;
+          let styles = window.getComputedStyle(this.item[0]);
+          let elWidth = this.item[0].getBoundingClientRect().width;
+          let promiseFunc = function () {
+            return new Promise(function (resolve) {
+
+            this.colMargin = parseInt(styles.marginRight.replace(/px/, ''));
+            this.itemWidth = elWidth + this.colMargin;
+
+            targetPosition = this.itemWidth * (this.itemLength - (this.column - (this.column - 1)));
+
+            this.slideInner.style.transitionDuration = '0s';
+            this.slideInner.style.left = '-' + targetPosition + 'px';
+
+            // 現在のカレントとleft位置を初期化
+            styles = window.getComputedStyle(this.slideInner);
+            this.isCurrentNum = this.itemLength;
+            this.nowPosition = parseInt(styles.left.match(/(\d+)/)[0], 10);
+            resolve();
+            });
+          };
+
+          promiseFunc().then(function () {
+            self.slideInner.style.transitionDuration = (self.duration / 1000) + 's';
+            self.isSliding = false;
+          });
+        },
+
+        /**
+         * 任意の箇所にスライドする処理
+         * @param {object} e - クリックされたインジケーター
+         * @return {void}
+         */
+        targetSlide: function (e) {
+          let targetNum = e.target.querySelector('.indicator-index').getAttribute('data-current');
+
+          if (this.isSliding) {
+            return;
+          }
+
+          this.isSliding = true;
+          this.resizeBeforeWidth = window.innerWidth;
+          this.indicatorUpdate(targetNum - 1);
+          this.isCurrentNum = parseInt(targetNum, 10);
+
+          if (this.animationType === 'slide') {
+            this.slideInner.style.left = '-' + (this.itemWidth * (targetNum - 1)) + 'px';
+          }
+
+          if (this.animationType === 'fade') {
+            this.item.forEach(function (el) {
+              el.style.opacity = 0;
+            });
+            this.item[this.isCurrentNum - 1].style.opacity = 1;
+          }
+        },
+
+        /**
+         * スライド時のタブインデックス操作
+         * @return {void}
+         */
+        changeTabIndex: function () {
+        },
 
       };
 
